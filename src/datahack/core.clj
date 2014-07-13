8d94b1db2c7ab1e420162849921c21655fc6a61c
Ivan Fraixedes <ifcdev@gmail.com>
2014-07-13 15:32:34 +0100

    Current main that use the current implemented ns

--- src/datahack/core.clj
+++ src/datahack/core.clj
@@ -1,11 +1,16 @@
 (ns datahack.core
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io])
+  (:require 
+    [datahack.quos :as quos]
+    [datahack.customer :as customer]
+    [datahack.traffic :as traffic])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-
+  (println "Hello, World!")
+  ;(println (quos/find-all :from "SQN"))
+  ;(println (customer/find-all :circuit "0JC-HK2-NP3"))
+  (println (traffic/find-all :minimum-commitment 500))
+  (println "End!!"))
 
